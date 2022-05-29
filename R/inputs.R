a51bac285d9d7ee152313a558230013c18175667
John Coene <jcoenep@gmail.com>
2022-05-29 12:49:03 +0200

    fix(button): triggers but not ideal

--- R/inputs.R
+++ R/inputs.R
@@ -5,22 +5,32 @@
 #' @param id ID of button and `input`.
 #' @param ... Content of button.
 #' @param class Any additional classes.
-#' @param .tag Tag to use, e.g.: `tags$a`.
+#' @param value Initial value of button, must be an integer.
 #' 
-#' @importFrom htmltools tagList
+#' @importFrom htmltools tagList tags
 #' 
 #' @export 
 button <- \(
   id,
   ...,
   class = ...class(),
-  .tag = htmltools::tags$button
+  .tag = htmltools::tags$button,
+  value = 0L
 ) {
+  cat("This likely will not work\n")
+
+  if(missing(id))
+    stop("Missing `id`", call. = FALSE)
+
+  if(!is.numeric(value))
+    stop("`value` must be a numeric", call. = FALSE)
+
   tagList(
     get_js_dependency("button"),
-    .tag(
+    tags$button(
       id = id,
       class = ...class("windy-button", class),
+      value = value,
       ...
     )
   )
