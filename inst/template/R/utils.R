2f0c36f13f57f01177d942ce1d67d94da4f019b4
John Coene <jcoenep@gmail.com>
2022-05-30 17:41:45 +0200

    fix: remove inputs: will not work

--- inst/template/R/utils.R
+++ inst/template/R/utils.R
@@ -1,3 +1,32 @@
+#' Package File
+#' 
+#' Get path to a file.
+#' 
+#' @param ... Passed to [system.file()].
+#' 
+#' @keywords internal
 pkg_file <- \(...){
-  system.file(..., package = "#PKG#")
+  system.file(..., package = "windy.app")
+}
+
+#' Create Select Choices
+#' 
+#' Create choices for a select input.
+#' 
+#' @param choices Choices, named vector or list.
+#' 
+#' @keywords internal
+select_choices <- \(
+  choices
+){
+  if(missing(choices))
+    stop("Missing `choices`", call. = FALSE)
+
+  labels <- names(choices)
+  values <- unname(choices)
+  labels[labels == ""] <- values[labels == ""]
+
+  names(values) <- labels
+
+  as.list(values)
 }
